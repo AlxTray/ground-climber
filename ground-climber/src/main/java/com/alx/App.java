e470ca9f37ea4bd5da9a05fa983bcdfed4b26e7a
AlxTray <alex.ashby02@icloud.com>
2023-05-02 22:19:40 +0100

    Replaced all basic JavaFX methods to FXGL
    Setup basic UI initialisation with FXGL,
     therefore removed controllers are fxml files

--- ground-climber/src/main/java/com/alx/App.java
+++ ground-climber/src/main/java/com/alx/App.java
@@ -1,38 +1,35 @@
 package com.alx;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
 
-import java.io.IOException;
+import javafx.scene.text.Text;
 
 /**
  * JavaFX App
  */
-public class App extends Application {
-
-    private static Scene scene;
+public class App extends GameApplication {
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Ground Climber");
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    @Override
+    public void initInput() {
+        Text text = new Text();
+        text.setText("Ground Climber");
+        text.setTranslateX(50);
+        text.setTranslateY(100);
+    
+        FXGL.getGameScene().addUINode(text);
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
