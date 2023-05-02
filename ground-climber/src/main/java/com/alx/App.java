f8def5338269a7f45c5d800adc8609fef1522127
AlxTray <alex.ashby02@icloud.com>
2023-05-02 23:04:30 +0100

    Fixed initUI(), and added initGame()
    initGame() currently creates basic player
    Also changed access modifiers of overrides to protected

--- ground-climber/src/main/java/com/alx/App.java
+++ ground-climber/src/main/java/com/alx/App.java
@@ -3,7 +3,10 @@ package com.alx;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 /**
@@ -11,15 +14,25 @@ import javafx.scene.text.Text;
  */
 public class App extends GameApplication {
 
+    private Entity player;
+
     @Override
-    public void initSettings(GameSettings settings) {
+    protected void initSettings(GameSettings settings) {
         settings.setWidth(800);
         settings.setHeight(600);
         settings.setTitle("Ground Climber");
     }
 
     @Override
-    public void initInput() {
+    protected void initGame() {
+        player = FXGL.entityBuilder()
+                .at(500, 500)
+                .view(new Rectangle(25, 25, Color.BLUE))
+                .buildAndAttach();
+    }
+
+    @Override
+    protected void initUI() {
         Text text = new Text();
         text.setText("Ground Climber");
         text.setTranslateX(50);
