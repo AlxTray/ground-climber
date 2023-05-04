4d82a4821560eb206ebff38b0847686aee5a9b58
AlxTray <alex.ashby02@icloud.com>
2023-05-04 20:56:48 +0100

    Added gravity property
    So that the player is accelerated downwards
    Also removed some further magic numbers

--- ground-climber/src/main/java/com/alx/App.java
+++ ground-climber/src/main/java/com/alx/App.java
@@ -29,13 +29,25 @@ public class App extends GameApplication {
     private final int jumpHeightInterval = 2;
     private final int baseJumpHeight = 75;
     private final double distanceJumpedDefault = 0.0;
-    private final int yAcceleratorMax = 3;
-    private final double yAcceleratorModifier = 0.008;
+    private final double yAcceleratorMax = 2.5;
+    private final double yAcceleratorModifier = 0.02;
     private final double yAcceleratorDefault = 1.0;
+
+    private final double gravityDefault = 1.0;
+    private final double gravityModifier = 0.1;
+    private final int gravityMax = 3;
+
+    private final int appSizeHeight = 600;
+    private final int appSizeWidth = 800;
+
+    private int playerSpawnX = 500;
+    private int playerSpawnY = 500;
+    private final int playerWidth = 25;
+    private final int playerHeight = 25;
 
     private PropertyMap gameVarsMap;
 
-    
+
     private Entity player;
 
     private UserAction moveLeft = new UserAction("Move Left") {
@@ -90,16 +102,16 @@ public class App extends GameApplication {
 
     @Override
     protected void initSettings(GameSettings settings) {
-        settings.setWidth(800);
-        settings.setHeight(600);
+        settings.setWidth(appSizeWidth);
+        settings.setHeight(appSizeHeight);
         settings.setTitle("Ground Climber");
     }
 
     @Override
     protected void initGame() {
         player = entityBuilder()
-                .at(500, 500)
-                .view(new Rectangle(25, 25, Color.BLUE))
+                .at(playerSpawnX, playerSpawnY)
+                .view(new Rectangle(playerWidth, playerHeight, Color.BLUE))
                 .buildAndAttach();
     }
 
@@ -116,6 +128,7 @@ public class App extends GameApplication {
     protected void initGameVars(Map<String, Object> vars) {
         vars.put("xAccelerator", xAcceleratorDefault);
         vars.put("yAccelerator", yAcceleratorDefault);
+        vars.put("gravity", gravityDefault);
         vars.put("jumping", false);
         vars.put("distanceJumped", distanceJumpedDefault);
 
@@ -147,7 +160,14 @@ public class App extends GameApplication {
         }
         // TODO: #3 Change current gravity stop collision with BLOCK entity, not bottom of the screen
         if (player.getY() <= getAppHeight() - 25) {
-            player.translateY(1);
+            double gravity = gameVarsMap.getDouble("gravity");
+            player.translateY(gravity);
+            if (gravity < gravityMax) {
+                gameVarsMap.setValue("gravity", gameVarsMap.getDouble("gravity") + gravityModifier);
+            }
+        }
+        else {
+            gameVarsMap.setValue("gravity", gravityDefault);
         }
     }
 
