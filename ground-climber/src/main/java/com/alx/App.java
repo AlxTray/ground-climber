01263c1d9488b07f37c55db0861a2fbd92a18915
AlxTray <alex.ashby02@icloud.com>
2023-05-08 17:26:26 +0100

    Added Physics and Component to the player entity
    Now has a groud sensor that has collision with platforms
    Added PlayerComponent class that handles player movement
    Updated gravity to use internal method not custom one
    Identified issue where lag slows movement after a few seconds

--- ground-climber/src/main/java/com/alx/App.java
+++ ground-climber/src/main/java/com/alx/App.java
@@ -8,9 +8,17 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.core.collection.PropertyMap;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.entity.components.IrremovableComponent;
 import com.almasb.fxgl.input.Input;
 import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
+import com.almasb.fxgl.physics.box2d.dynamics.FixtureDef;
 
+import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -21,21 +29,19 @@ import javafx.scene.text.Text;
  */
 public class App extends GameApplication {
 
-    private final int basePlayerSpeed = 2;
+    private final int basePlayerSpeed = 100;
     private final double xAcceleratorMax = 2.2;
     private final double xAcceleratorModifier = 0.008;
     private final double xAcceleratorDefault = 1.0;
 
-    private final int jumpHeightInterval = 2;
-    private final int baseJumpHeight = 75;
+    private final int jumpHeightInterval = 100;
+    private final int baseJumpHeight = 5000;
     private final double distanceJumpedDefault = 0.0;
     private final double yAcceleratorMax = 2.5;
     private final double yAcceleratorModifier = 0.02;
     private final double yAcceleratorDefault = 1.0;
 
-    private final double gravityDefault = 1.0;
-    private final double gravityModifier = 0.15;
-    private final int gravityMax = 3;
+    private final double gravityDefault = 300;
 
     private final int appSizeHeight = 600;
     private final int appSizeWidth = 800;
@@ -47,6 +53,9 @@ public class App extends GameApplication {
 
     private PropertyMap gameVarsMap;
 
+    private enum EntityType {
+        PLAYER, PLATFORM, COIN, ENEMY, DEATH
+    }
 
     private Entity player;
 
@@ -55,7 +64,7 @@ public class App extends GameApplication {
         protected void onAction() {
             double xAccelerator = gameVarsMap.getDouble("xAccelerator");
 
-            player.translateX(-basePlayerSpeed * xAccelerator);
+            player.getComponent(PlayerComponent.class).moveLeft(basePlayerSpeed * xAccelerator);
             if (xAccelerator < xAcceleratorMax) {
                 gameVarsMap.setValue("xAccelerator", xAccelerator + xAcceleratorModifier);
             }
@@ -63,6 +72,7 @@ public class App extends GameApplication {
 
         @Override
         protected void onActionEnd() {
+            player.getComponent(PlayerComponent.class).stopX();
             gameVarsMap.setValue("xAccelerator", xAcceleratorDefault);
         }
     };
@@ -72,7 +82,7 @@ public class App extends GameApplication {
         protected void onAction() {
             double xAccelerator = gameVarsMap.getDouble("xAccelerator");
 
-            player.translateX(basePlayerSpeed * xAccelerator);
+            player.getComponent(PlayerComponent.class).moveRight(basePlayerSpeed * xAccelerator);
             if (xAccelerator < xAcceleratorMax) {
                 gameVarsMap.setValue("xAccelerator", xAccelerator + xAcceleratorModifier);
             }
@@ -80,6 +90,7 @@ public class App extends GameApplication {
 
         @Override
         protected void onActionEnd() {
+            player.getComponent(PlayerComponent.class).stopX();
             gameVarsMap.setValue("xAccelerator", xAcceleratorDefault);
         }
     };
@@ -104,13 +115,25 @@ public class App extends GameApplication {
         settings.setWidth(appSizeWidth);
         settings.setHeight(appSizeHeight);
         settings.setTitle("Ground Climber");
+
+        settings.setDeveloperMenuEnabled(true);
     }
 
     @Override
     protected void initGame() {
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.DYNAMIC);
+        physics.addGroundSensor(new HitBox("GroundSensor", new Point2D(0, 0), BoundingShape.box(1, 1)));
+        physics.setFixtureDef(new FixtureDef().friction(0.0f));
+
         player = entityBuilder()
+                .type(EntityType.PLAYER)
                 .at(playerSpawnX, playerSpawnY)
-                .view(new Rectangle(playerWidth, playerHeight, Color.BLUE))
+                .viewWithBBox(new Rectangle(playerWidth, playerHeight, Color.BLUE))
+                .with(physics)
+                .with(new CollidableComponent(true))
+                .with(new IrremovableComponent())
+                .with(new PlayerComponent())
                 .buildAndAttach();
 
         getGameWorld().addEntityFactory(new ProceduralLevelFactory());
@@ -131,7 +154,6 @@ public class App extends GameApplication {
     protected void initGameVars(Map<String, Object> vars) {
         vars.put("xAccelerator", xAcceleratorDefault);
         vars.put("yAccelerator", yAcceleratorDefault);
-        vars.put("gravity", gravityDefault);
         vars.put("jumping", false);
         vars.put("distanceJumped", distanceJumpedDefault);
 
@@ -149,28 +171,25 @@ public class App extends GameApplication {
     }
 
     @Override
+    protected void initPhysics() {
+        getPhysicsWorld().setGravity(0, gravityDefault);
+    }
+
+    @Override
     protected void onUpdate(double tpf) {
-        if (gameVarsMap.getBoolean("jumping")) {
-            final double yAccelerator = gameVarsMap.getDouble("yAccelerator");
-            final double amountToJump = -jumpHeightInterval * yAccelerator;
-            player.translateY(amountToJump);
-            inc("distanceJumped", amountToJump);
-            if (gameVarsMap.getDouble("distanceJumped") <= -baseJumpHeight * yAccelerator) {
-                gameVarsMap.setValue("jumping", false);
-                gameVarsMap.setValue("yAccelerator", yAcceleratorDefault);
-                gameVarsMap.setValue("distanceJumped", distanceJumpedDefault);
-            }
+        if (!gameVarsMap.getBoolean("jumping")) {
+            return;
         }
-        // TODO: #3 Change current gravity stop collision with BLOCK entity, not bottom of the screen
-        if (player.getY() <= getAppHeight() - 25) {
-            double gravity = gameVarsMap.getDouble("gravity");
-            player.translateY(gravity);
-            if (gravity < gravityMax) {
-                gameVarsMap.setValue("gravity", gravity + gravityModifier);
-            }
-        }
-        else {
-            gameVarsMap.setValue("gravity", gravityDefault);
+
+        final double yAccelerator = gameVarsMap.getDouble("yAccelerator");
+        final double amountToJump = jumpHeightInterval * yAccelerator;
+        player.getComponent(PlayerComponent.class).jump(amountToJump);
+        inc("distanceJumped", amountToJump);
+        if (gameVarsMap.getDouble("distanceJumped") >= baseJumpHeight * yAccelerator) {
+            player.getComponent(PlayerComponent.class).stopY();
+            gameVarsMap.setValue("jumping", false);
+            gameVarsMap.setValue("yAccelerator", yAcceleratorDefault);
+            gameVarsMap.setValue("distanceJumped", distanceJumpedDefault);
         }
     }
 
--- ground-climber/src/main/java/com/alx/ProceduralLevelFactory.java
+++ ground-climber/src/main/java/com/alx/ProceduralLevelFactory.java
@@ -6,6 +6,8 @@ import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.physics.PhysicsComponent;
 
 import javafx.scene.shape.Rectangle;
 
@@ -14,12 +16,12 @@ public class ProceduralLevelFactory implements EntityFactory {
     private final int[][] startingPlatformCoords = {{0, 550}, {100, 550}, {150, 550}, {230, 550}, {300, 550}, {400, 550}, {450, 550}, {500, 550}, {650, 550}};
 
     private enum EntityType {
-        PLATFORM, COIN, ENEMY, DEATH
+        PLAYER, PLATFORM, COIN, ENEMY, DEATH
     }
 
     public void spawnStartingPlatforms() {
         for (int[] coords : startingPlatformCoords) { 
-            spawn("platform", coords[0], coords[1]);
+            getGameWorld().spawn("platform", coords[0], coords[1]);
         }
     }
 
@@ -28,6 +30,7 @@ public class ProceduralLevelFactory implements EntityFactory {
         return entityBuilder(data)
             .type(EntityType.PLATFORM)
             .viewWithBBox(new Rectangle(50, 50))
+            .with(new PhysicsComponent())
             .build();
     }
 }
