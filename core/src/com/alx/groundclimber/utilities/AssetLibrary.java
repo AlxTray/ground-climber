9b5017d6e5626b5830a0facc09a73d520cab5b42
AlxTray <alex.ashby02@icloud.com>
2024-09-12 21:17:52 +0100

    feat: added loading for currently used assets
    except the platform texture as this will be made redundant

--- core/src/com/alx/groundclimber/utilities/AssetLibrary.java
+++ core/src/com/alx/groundclimber/utilities/AssetLibrary.java
@@ -2,11 +2,21 @@ package com.alx.groundclimber.utilities;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.SkinLoader;
+import com.badlogic.gdx.assets.loaders.TextureLoader;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.ObjectMap;
 
 public class AssetLibrary {
     
     private AssetManager assetManager;
+    private ObjectMap<String, String> filePathAliases;
+    
+    private String skinAtlasPath = "skin/uiskin.atlas";
+    private String skinJsonPath = "skin/uiskin.json";
+    private String playerImagePath = "player.png";
+    private String playerFaceImagePath = "player_face.png";
+    private String backgroundImagePath = "background.png";
 
     private AssetLibrary() {
     }
@@ -21,14 +31,27 @@ public class AssetLibrary {
     
     public void init() {
         assetManager = new AssetManager();
+        filePathAliases = new ObjectMap<>();
         loadInitialAssets();
     }
     
     private void loadInitialAssets() {
-        SkinLoader.SkinParameter parameter = new SkinLoader.SkinParameter("skin/uiskin.atlas");
-        assetManager.load("skin/uiskin.json", Skin.class, parameter);
+        SkinLoader.SkinParameter parameter = new SkinLoader.SkinParameter(skinAtlasPath);
+        assetManager.load(skinJsonPath, Skin.class, parameter);
+        filePathAliases.put("skin", skinJsonPath);
+        
+        TextureLoader.TextureParameter textureParameter = new TextureLoader.TextureParameter();
+        assetManager.load(playerImagePath, Texture.class, textureParameter);
+        filePathAliases.put("player", playerImagePath);
+        assetManager.load(playerFaceImagePath, Texture.class, textureParameter);
+        filePathAliases.put("playerFace", playerFaceImagePath);
     }
     
+    private void loadGeneralLevelAssets() {
+        TextureLoader.TextureParameter textureParameter = new TextureLoader.TextureParameter();
+        assetManager.load(backgroundImagePath, Texture.class, textureParameter);
+        filePathAliases.put("background", backgroundImagePath);
+    }
     
     public AssetManager getAssetManager() {
         return assetManager;
