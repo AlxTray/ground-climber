ba277b7250d7faf77d9a2aa8fffad3e256904bdc
AlxTray <alex.ashby02@icloud.com>
2024-09-22 16:21:18 +0100

    refactor: introduced ControllerManager and RenderManager to abstract away from GameScreen

---
+++ core/src/main/java/io/github/alxtray/groundclimber/renderers/RenderManager.java
@@ -0,0 +1,78 @@
+package io.github.alxtray.groundclimber.renderers;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
+import io.github.alxtray.groundclimber.bodies.EnvironmentObject;
+import io.github.alxtray.groundclimber.bodies.Player;
+import io.github.alxtray.groundclimber.enums.DebugRenderMode;
+import io.github.alxtray.groundclimber.utilities.Logger;
+import io.github.alxtray.groundclimber.enums.LogLevel;
+import text.formic.Stringf;
+
+public class RenderManager {
+    private final BitmapFont font;
+    private final PlayerRenderer playerRenderer;
+    private final BackgroundObjectRenderer backgroundObjectRenderer;
+    private final EnvironmentObjectVisitor environmentObjectRenderer;
+    private boolean displayDebugInfo;
+
+    public RenderManager() {
+        font = new BitmapFont();
+        playerRenderer = new PlayerRenderer();
+        backgroundObjectRenderer = new BackgroundObjectRenderer();
+        environmentObjectRenderer = new EnvironmentObjectRenderer();
+        displayDebugInfo = false;
+    }
+
+    public void render(OrthographicCamera camera, Player player, Array<EnvironmentObject> environmentObjects, SpriteBatch batch) {
+        batch.setProjectionMatrix(camera.combined);
+        backgroundObjectRenderer.render(batch, camera);
+        playerRenderer.render(batch, player);
+        for (EnvironmentObject environmentObject : environmentObjects) {
+            environmentObject.acceptRender(environmentObjectRenderer, batch);
+        }
+        if (displayDebugInfo) {
+            drawDebugInfo(camera, player, batch);
+        }
+    }
+
+    private void drawDebugInfo(OrthographicCamera camera, Player player, SpriteBatch batch) {
+        float cornerX = (camera.position.x - camera.viewportWidth / 2) + 10;
+        float cornerY = camera.position.y + camera.viewportHeight / 2;
+        font.draw(batch, "FPS: " + Gdx.graphics.getFramesPerSecond(), cornerX, cornerY - 10);
+        font.draw(
+            batch,
+            Stringf.format(
+                "Player Pos: (%.2f, %.2f)",
+                player.getBody().getPosition().x,
+                player.getBody().getPosition().y),
+            cornerX,
+            cornerY - 30);
+        font.draw(batch,
+            Stringf.format(
+                "Player Lin Vec: (%.2f, %.2f)",
+                player.getBody().getLinearVelocity().x,
+                player.getBody().getLinearVelocity().y),
+            cornerX,
+            cornerY - 50);
+        font.draw(batch,
+            Stringf.format(
+                "Player Ang Vec: %.2f",
+                player.getBody().getAngularVelocity()),
+            cornerX,
+            cornerY - 70);
+    }
+
+    public void toggleDebugInfo() {
+        displayDebugInfo = !displayDebugInfo;
+    }
+
+    public void dispose() {
+        font.dispose();
+        Logger.log("RenderManager", "Disposed objects", LogLevel.DEBUG);
+    }
+
+}
--- core/src/main/java/io/github/alxtray/groundclimber/screens/GameScreen.java
+++ core/src/main/java/io/github/alxtray/groundclimber/screens/GameScreen.java
@@ -3,56 +3,38 @@ package io.github.alxtray.groundclimber.screens;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.utils.ObjectIntMap;
-import io.github.alxtray.groundclimber.bodies.EnvironmentObject;
 import io.github.alxtray.groundclimber.bodies.Player;
-import io.github.alxtray.groundclimber.controllers.CameraController;
-import io.github.alxtray.groundclimber.controllers.PhysicsController;
-import io.github.alxtray.groundclimber.controllers.PlayerController;
+import io.github.alxtray.groundclimber.controllers.ControllerManager;
 import io.github.alxtray.groundclimber.enums.DebugRenderMode;
 import io.github.alxtray.groundclimber.enums.GameMode;
 import io.github.alxtray.groundclimber.enums.LogLevel;
 import io.github.alxtray.groundclimber.level.LevelData;
-import io.github.alxtray.groundclimber.renderers.BackgroundObjectRenderer;
-import io.github.alxtray.groundclimber.renderers.EnvironmentObjectRenderer;
-import io.github.alxtray.groundclimber.renderers.EnvironmentObjectVisitor;
-import io.github.alxtray.groundclimber.renderers.PlayerRenderer;
+import io.github.alxtray.groundclimber.renderers.RenderManager;
 import io.github.alxtray.groundclimber.utilities.AssetLibrary;
-import io.github.alxtray.groundclimber.utilities.EndlessPlatformGenerator;
 import io.github.alxtray.groundclimber.utilities.Logger;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.ScreenUtils;
 import text.formic.Stringf;
 
 public class GameScreen implements Screen {
-    private final GameMode gameMode;
     private final DebugRenderMode renderMode;
-    private boolean displayDebugInfo;
     private final SpriteBatch batch;
-    private final BitmapFont font;
     private final Box2DDebugRenderer debugRenderer;
-    private final EndlessPlatformGenerator platformGenerator;
-    private final CameraController cameraController;
-    private final PhysicsController physicsController;
-    private final PlayerController playerController;
-    private final PlayerRenderer playerRenderer;
-    private final BackgroundObjectRenderer backgroundObjectRenderer;
-    private final EnvironmentObjectVisitor environmentObjectRenderer;
+    private final ControllerManager controllerManager;
+    private final RenderManager renderManager;
 
     public GameScreen(GameMode gameMode, DebugRenderMode renderMode, String... selectedLevelNames) {
-        this.gameMode = gameMode;
         this.renderMode = renderMode;
-        displayDebugInfo = false;
         batch = new SpriteBatch();
-        font = new BitmapFont();
+        debugRenderer =
+            (renderMode.equals(DebugRenderMode.ONLY) || renderMode.equals(DebugRenderMode.OVERLAY))
+                ? new Box2DDebugRenderer() : null;
         // Loads all assets that are required for all levels
         AssetLibrary.getInstance().loadGeneralLevelAssets();
         Logger.log(
@@ -67,42 +49,14 @@ public class GameScreen implements Screen {
                 "GameScreen",
                 "Finished loading general level assets",
                 LogLevel.INFO);
-
         Logger.log(
-                "GameScreen",
-                Stringf.format("The current game mode is: %s", gameMode.name()),
-                LogLevel.INFO);
-        Json json = new Json();
-        LevelData levelData = null;
-        if (gameMode == GameMode.NORMAL) {
-            Array<FileHandle> selectedLevelFiles = new Array<>();
-            for (String levelName : selectedLevelNames) {
-                selectedLevelFiles.add(Gdx.files.internal("levels/" + levelName));
-            }
-            levelData = json.fromJson(LevelData.class, selectedLevelFiles.first().readString());
-        } else if (gameMode == GameMode.ENDLESS) {
-            levelData = json.fromJson(LevelData.class, Gdx.files.internal("endless.json"));
-            Logger.log(
-                    "GameScreen",
-                    "Empty levelData successfully created for ENDLESS mode",
-                    LogLevel.INFO);
-        }
-        cameraController = new CameraController(levelData.getCameraStartPosition(), levelData.getBounds());
-        physicsController = new PhysicsController(levelData.getPlatformsData());
-        if (gameMode.equals(GameMode.ENDLESS)) {
-            platformGenerator = new EndlessPlatformGenerator();
-        } else {
-            platformGenerator = null;
-        }
-        playerController = new PlayerController(physicsController.getWorld(), levelData.getPlayerSpawn());
-        if (renderMode.equals(DebugRenderMode.ONLY) || renderMode.equals(DebugRenderMode.OVERLAY)) {
-            debugRenderer = new Box2DDebugRenderer();
-        } else {
-            debugRenderer = null;
-        }
-        playerRenderer = new PlayerRenderer();
-        backgroundObjectRenderer = new BackgroundObjectRenderer();
-        environmentObjectRenderer = new EnvironmentObjectRenderer();
+            "GameScreen",
+            Stringf.format("The current game mode is: %s", gameMode.name()),
+            LogLevel.INFO);
+
+        LevelData levelData = loadLevelData(gameMode, selectedLevelNames);
+        controllerManager = new ControllerManager(gameMode, levelData);
+        renderManager = new RenderManager();
     }
 
     @Override
@@ -110,65 +64,41 @@ public class GameScreen implements Screen {
         ScreenUtils.clear(Color.BLACK);
         handleInput();
 
-        if (gameMode.equals(GameMode.ENDLESS)) {
-            platformGenerator.checkAndGenerateBatch(
-                physicsController.getWorld(),
-                playerController.getPlayer(),
-                physicsController.getEnvironmentObjects());
-        }
-
-        playerController.update(delta);
+        controllerManager.update(delta);
         checkPlayerInBounds();
 
-        Player player = playerController.getPlayer();
-        cameraController.update(delta, player, gameMode);
-        physicsController.step(delta);
-
-        OrthographicCamera camera = cameraController.getCamera();
+        OrthographicCamera camera = controllerManager.getCamera();
+        Player player = controllerManager.getPlayer();
         if (!renderMode.equals(DebugRenderMode.ONLY)) {
             batch.begin();
-            batch.setProjectionMatrix(camera.combined);
-            backgroundObjectRenderer.render(batch, camera);
-            playerRenderer.render(batch, player);
-            for (EnvironmentObject environmentObject : physicsController.getEnvironmentObjects()) {
-                environmentObject.acceptRender(environmentObjectRenderer, batch);
-            }
-            if (displayDebugInfo) {
-                drawDebugInfo(camera, player);
-            }
+            renderManager.render(camera, player, controllerManager.getEnvironmentObjects(), batch);
             batch.end();
         }
         if (renderMode.equals(DebugRenderMode.ONLY) || renderMode.equals(DebugRenderMode.OVERLAY)) {
-            debugRenderer.render(physicsController.getWorld(), camera.combined);
+            debugRenderer.render(controllerManager.getWorld(), camera.combined);
         }
+    }
+
+    private LevelData loadLevelData(GameMode gameMode, String... selectedLevelNames) {
+        final Json json = new Json();
+        if (gameMode.equals(GameMode.NORMAL)) {
+            return json.fromJson(LevelData.class, Gdx.files.internal("levels/" + selectedLevelNames[0]).readString());
+        } else if (gameMode.equals(GameMode.ENDLESS)) {
+            Logger.log("GameScreen", "Empty levelData successfully created for ENDLESS mode", LogLevel.INFO);
+            return json.fromJson(LevelData.class, Gdx.files.internal("endless.json").readString());
+        }
+        return null;
     }
 
     private void handleInput() {
         if (Gdx.input.isKeyJustPressed(Input.Keys.F3)) {
-            displayDebugInfo = !displayDebugInfo;
+            renderManager.toggleDebugInfo();
         }
     }
 
-    private void drawDebugInfo(OrthographicCamera camera, Player player) {
-        // 10 added as each line will be 10 pixels away from left anyway
-        float cornerX = (camera.position.x - camera.viewportWidth / 2) + 10;
-        float cornerY = camera.position.y + camera.viewportHeight / 2;
-        font.draw(batch, "FPS: " + Gdx.graphics.getFramesPerSecond(), cornerX, cornerY - 10);
-        font.draw(batch,
-            Stringf.format(
-                "Player Pos: (%.2f, %.2f)",
-                player.getBody().getPosition().x,
-                player.getBody().getPosition().y),
-            cornerX, cornerY - 30);
-        font.draw(batch, Stringf.format("Player Lin Vec: (%.2f, %.2f)", player.getBody().getLinearVelocity().x,
-            player.getBody().getLinearVelocity().y), cornerX, cornerY - 50);
-        font.draw(batch, Stringf.format("Player Ang Vec: %.2f", player.getBody().getAngularVelocity()), cornerX,
-            cornerY - 70);
-    }
-
     private void checkPlayerInBounds() {
-        Player player = playerController.getPlayer();
-        ObjectIntMap<String> bounds = cameraController.getBounds();
+        Player player = controllerManager.getPlayer();
+        ObjectIntMap<String> bounds = controllerManager.getBounds();
         // Kill player if they leave map bounds
         if (player.getBody().getPosition().x < bounds.get("left", 0)
             || player.getPosition().x > bounds.get("right", 0)
@@ -205,7 +135,7 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         batch.dispose();
-        font.dispose();
+        renderManager.dispose();
         Logger.log(
                 "GameScreen",
                 "Disposed objects",
